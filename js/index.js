4491820bfd53001c5249f0ece22118eb72938852
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2023-07-11 14:48:57 +0300

    Added display items and shuffle them

--- js/index.js
+++ js/index.js
@@ -23,21 +23,44 @@ let fruitsJSON = `[
 // преобразование JSON в объект JavaScript
 let fruits = JSON.parse(fruitsJSON);
 
+
 /*** ОТОБРАЖЕНИЕ ***/
+
+let colors = new Map();
+colors.set('фиолетовый', 'violet');
+colors.set('зеленый', 'green');
+colors.set('розово-красный', 'carmazin');
+colors.set('желтый', 'yellow');
+colors.set('светло-коричневый', 'lightbrown');
 
 // отрисовка карточек
 const display = () => {
   // TODO: очищаем fruitsList от вложенных элементов,
   // чтобы заполнить актуальными данными из fruits
+  fruitsList.innerHTML = '';
 
   for (let i = 0; i < fruits.length; i++) {
     // TODO: формируем новый элемент <li> при помощи document.createElement,
     // и добавляем в конец списка fruitsList при помощи document.appendChild
+    let fruit = document.createElement('li');
+    fruit.className = 'fruit__item ';
+    fruit.className += 'fruit_' + colors.get(fruits[i].color);
+    fruit.innerHTML = `
+      <div class="fruit__info">
+        <div>index: ${i}</div>
+        <div>kind: ${fruits[i].kind}</div>
+        <div>color: ${fruits[i].color}</div>
+        <div>weight (кг): ${fruits[i].weight}</div>
+      </div>
+    `;
+    fruitsList.appendChild(fruit);
   }
 };
 
 // первая отрисовка карточек
-display();
+window.onload = function(){
+  display();
+};
 
 /*** ПЕРЕМЕШИВАНИЕ ***/
 
@@ -58,6 +81,10 @@ const shuffleFruits = () => {
     // вырезаем его из fruits и вставляем в result.
     // ex.: [1, 2, 3], [] => [1, 3], [2] => [3], [2, 1] => [], [2, 1, 3]
     // (массив fruits будет уменьшатся, а result заполняться)
+
+    let randIndex = getRandomInt(0, fruits.length - 1);
+    let randElem = randIndex == 0 ? fruits.splice(0, 1) : fruits.splice(randIndex, 1);
+    result.push(randElem[0]);
   }
 
   fruits = result;
